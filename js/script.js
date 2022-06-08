0970e21993da95961eeb887f284786af3d4c7932
m-colen <mzcolen@gmail.com>
2022-06-07 22:36:08 -0500

    Refactored HTML, CSS, & JS for Nav section

--- js/script.js
+++ js/script.js
@@ -1,106 +1,140 @@
-/* Nav Section */
+/* 
+ * Nav Section 
+ */
 
-const hamMenu = document.getElementById('hamburger-menu');
-const topBun = document.getElementById('ham-top-bun');
-const bottomBun = document.getElementById('ham-bottom-bun');
-const mobileNavMenu = document.getElementById('nav-links-mobile');
-const navLinks = document.getElementById('nav-links');
+// Toggles ham menu
 
-// Toggle hamburger menu 
-const hamMenuToggle = () => {
-  topBun.className === 'hamburger-item' || topBun.className === 'hamburger-item start' ? topBun.className = 'hamburger-item ham-top-inactive' : topBun.className = 'hamburger-item';
-  bottomBun.className === 'hamburger-item' || bottomBun.className === 'hamburger-item start' ? bottomBun.className = 'hamburger-item ham-bottom-inactive' : bottomBun.className = 'hamburger-item';
-  mobileNavMenu.style.display === 'none' || topBun.className === 'hamburger-item ham-top-inactive' ? mobileNavMenu.style.display = 'block' : mobileNavMenu.style.display = 'none'
+const hamMenu = document.querySelector(".hamburger-menu"); // Target
+
+const toggleHamburgerMenu = () => {
+  const hamTopBun = document.querySelector(".ham-top-bun");
+  const hamBottomBun = document.querySelector(".ham-bottom-bun");
+  const mobileNav = document.querySelector("#nav-links-mobile");
+
+  // Toggles css class to apply transition effect
+  hamTopBun.classList.toggle("ham-top-inactive");
+  hamBottomBun.classList.toggle("ham-bottom-inactive");
+
+  // Toggles mobile nav on click
+  mobileNav.classList.toggle("active");
 };
 
-hamMenu.addEventListener('click', hamMenuToggle);
+hamMenu.addEventListener("click", toggleHamburgerMenu);
 
-// To hide mobile nav menu on selection 
-const mobileAbout = document.getElementById('about-m');
-const mobileProjects = document.getElementById('projects-m');
-const mobileContact = document.getElementById('contact-m');
+// To hide mobile nav menu on selection
+const mobileAbout = document.getElementById("about-m");
+const mobileProjects = document.getElementById("projects-m");
+const mobileContact = document.getElementById("contact-m");
 
-mobileAbout.addEventListener('click', hamMenuToggle);
-mobileProjects.addEventListener('click', hamMenuToggle);
-mobileContact.addEventListener('click', hamMenuToggle);
+mobileAbout.addEventListener("click", toggleHamburgerMenu);
+mobileProjects.addEventListener("click", toggleHamburgerMenu);
+mobileContact.addEventListener("click", toggleHamburgerMenu);
 
 // Hides mobile nav when width passes 825px threshold
 const resetNav = () => {
+  const mobileNav = document.querySelector("#nav-links-mobile");
+  const hamTopBun = document.querySelector(".ham-top-bun");
+  const hamBottomBun = document.querySelector(".ham-bottom-bun");
   if (window.innerWidth >= 825) {
-    mobileNavMenu.style.display = 'none';
-    topBun.className = 'hamburger-item';
-    bottomBun.className = 'hamburger-item';
+    mobileNav.classList.remove("active");
+    hamTopBun.classList.toggle('ham-top-inactive');
+    hamBottomBun.classList.toggle('ham-bottom-inactive');
   }
 };
 
-window.addEventListener('resize', resetNav);
+window.addEventListener("resize", resetNav);
 
-/* About me section */
+/* 
+ * About me section 
+ */
 
 // Adds functionality to the "about" button to show/hide text
-const aboutMeSection = document.getElementById('about-text');
-const aboutButton = document.getElementById('about-me-button');
-const aboutMeClosed = document.getElementById('about-me-button-close')
-const mainNavAbout = document.getElementById('main-nav-about');
-const mobileNavAbout = document.getElementById('about-m');
-const aboutCard = document.getElementById('about-card');
+const aboutMeSection = document.getElementById("about-text");
+const aboutButton = document.getElementById("about-me-button");
+const aboutMeClosed = document.getElementById("about-me-button-close");
+const mainNavAbout = document.querySelector(".nav-about-link");
+const mobileNavAbout = document.getElementById("about-m");
+const aboutCard = document.getElementById("about-card");
 
 const aboutButtonToggle = () => {
   // To toggle "about me" text
-  if (aboutMeSection.className === 'inactive' && window.innerWidth > 1175) {
-    aboutMeSection.className = 'active unhidden-large';
-  } else if (aboutMeSection.className === 'inactive' && window.innerWidth <= 1175) {
-    aboutMeSection.className = 'active unhidden-small';
+  if (aboutMeSection.className === "inactive" && window.innerWidth > 1175) {
+    aboutMeSection.className = "active unhidden-large";
+  } else if (
+    aboutMeSection.className === "inactive" &&
+    window.innerWidth <= 1175
+  ) {
+    aboutMeSection.className = "active unhidden-small";
   } else {
-    aboutMeSection.className = 'inactive';
+    aboutMeSection.className = "inactive";
   }
-  aboutMeClosed.className === 'inactive' ? aboutMeClosed.className = '' : aboutMeClosed.className = 'inactive';
-  aboutCard.className === '' ? aboutCard.className = 'about-card-minimal' : aboutCard.className = '';
+  aboutMeClosed.className === "inactive"
+    ? (aboutMeClosed.className = "")
+    : (aboutMeClosed.className = "inactive");
+  aboutCard.className === ""
+    ? (aboutCard.className = "about-card-minimal")
+    : (aboutCard.className = "");
 };
 
-aboutButton.addEventListener('click', aboutButtonToggle);
-mainNavAbout.addEventListener('click', aboutButtonToggle);
-mobileNavAbout.addEventListener('click', aboutButtonToggle);
-aboutMeClosed.addEventListener('click', aboutButtonToggle);
+aboutButton.addEventListener("click", aboutButtonToggle);
+mainNavAbout.addEventListener("click", aboutButtonToggle);
+mobileNavAbout.addEventListener("click", aboutButtonToggle);
+aboutMeClosed.addEventListener("click", aboutButtonToggle);
 
 /* Project carousel */
 
 // Targets
-const leftArrow = document.getElementById('left-arrow');
-const rightArrow = document.getElementById('right-arrow');
+const leftArrow = document.getElementById("left-arrow");
+const rightArrow = document.getElementById("right-arrow");
 
 // Variables needing updated on each project change
-const projectDisplay = document.getElementById('project-display');
-const projectSummaryText = document.getElementById('project-summary-text');
-const githubButton = document.getElementById('project-github-button');
-const liveButton = document.getElementById('project-live-button');
+const projectDisplay = document.getElementById("project-display");
+const projectSummaryText = document.getElementById("project-summary-text");
+const githubButton = document.getElementById("project-github-button");
+const liveButton = document.getElementById("project-live-button");
 
 // Arrays for project data
-const projectSites = ['https://m-colen.github.io/zen-coffee/','https://m-colen.github.io/number-guessor/'];
-const projectDescriptions = ['This project is the landing page for a fictional coffee shop called Zen Coffee. **In Progress','This project is a number guessing game in which you challenge the computer to see who can get closer to a secret number.'];
-const githubPageLinks = ['https://github.com/m-Colen/zen-coffee','https://github.com/m-Colen/number-guessor'];
-const liveProjectLinks = ['https://m-colen.github.io/zen-coffee/','https://m-colen.github.io/number-guessor/'];
+const projectSites = [
+  "https://m-colen.github.io/zen-coffee/",
+  "https://m-colen.github.io/number-guessor/",
+];
+const projectDescriptions = [
+  "This project is the landing page for a fictional coffee shop called Zen Coffee. **In Progress",
+  "This project is a number guessing game in which you challenge the computer to see who can get closer to a secret number.",
+];
+const githubPageLinks = [
+  "https://github.com/m-Colen/zen-coffee",
+  "https://github.com/m-Colen/number-guessor",
+];
+const liveProjectLinks = [
+  "https://m-colen.github.io/zen-coffee/",
+  "https://m-colen.github.io/number-guessor/",
+];
 
-// To increment/decrement project count on each interaction 
+// To increment/decrement project count on each interaction
 let projectNumber = 0;
 
 // Moves to next project in arr
 const moveRight = () => {
-  projectNumber < projectSites.length - 1 ? projectNumber++ : projectNumber = 0;
-  projectDisplay.setAttribute('src', projectSites[projectNumber]);
+  projectNumber < projectSites.length - 1
+    ? projectNumber++
+    : (projectNumber = 0);
+  projectDisplay.setAttribute("src", projectSites[projectNumber]);
   projectSummaryText.innerHTML = projectDescriptions[projectNumber];
-  githubButton.setAttribute('href', githubPageLinks[projectNumber]);
-  liveButton.setAttribute('href', liveProjectLinks[projectNumber]);
+  githubButton.setAttribute("href", githubPageLinks[projectNumber]);
+  liveButton.setAttribute("href", liveProjectLinks[projectNumber]);
 };
 
 // Moves to previous project in arr
 const moveLeft = () => {
-  projectNumber >= 1 ? projectNumber-- : projectNumber = projectSites.length - 1;
-  projectDisplay.setAttribute('src', projectSites[projectNumber]);
+  projectNumber >= 1
+    ? projectNumber--
+    : (projectNumber = projectSites.length - 1);
+  projectDisplay.setAttribute("src", projectSites[projectNumber]);
   projectSummaryText.innerHTML = projectDescriptions[projectNumber];
-  githubButton.setAttribute('href', githubPageLinks[projectNumber]);
-  liveButton.setAttribute('href', liveProjectLinks[projectNumber]);
+  githubButton.setAttribute("href", githubPageLinks[projectNumber]);
+  liveButton.setAttribute("href", liveProjectLinks[projectNumber]);
 };
 
-rightArrow.addEventListener('click', moveRight);
-leftArrow.addEventListener('click', moveLeft);
+rightArrow.addEventListener("click", moveRight);
+leftArrow.addEventListener("click", moveLeft);
